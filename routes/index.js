b8e8732bceaec03710b7f216d5ed3f323ad8a6af
YanMer1 <mer21002@byui.edu>
2023-10-08 01:02:18 +0100

    Week04

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ routes.get('/awesome', myController.returnAnotherPerson);*/
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
